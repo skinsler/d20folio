28ab1fa00ad59b81b2a8ea77f6ad7d0689d27f4f
skinsler <simonkinsler@gmail.com>
2019-06-26 13:50:34 -0500

    Retrieve all Characters and Create Character routes now save to the database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,11 +4,21 @@ const seeds = require('../charSeeds.json');
 
 router.route('/characters')
     .get((req,res,err) => {
-        res.json(seeds);
+        // res.json(seeds);
+        db.Character.find({})
+            .sort({_id: -1})
+            .then(characters => {console.log ("Got characters: ", characters); return characters})
+            .then(characters => res.json(characters))
+            .catch(err => res.json(500, err))
     })
 router.route('/character')
     .post((req,res,err) => {
-        res.json("");
+        // res.json("");
+        const newChar = req.body;
+
+        db.Character.create(newChar)
+            .then(character => res.json(character))
+            .catch(err => res.json(500, error))
     })
 router.route('/character/:id')
     .get((req,res,err) => {
